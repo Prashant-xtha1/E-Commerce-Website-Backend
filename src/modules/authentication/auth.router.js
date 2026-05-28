b8fe2019713bc71b8e9c9a29d8a58dd951856938
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-05-28 14:24:47 +0545

    validation schema add

--- src/modules/authentication/auth.router.js
+++ src/modules/authentication/auth.router.js
@@ -1,10 +1,14 @@
+const checkLogin = require("../../middlewares/auth.middleware");
+const bodyValidator = require("../../middlewares/validation.middleware");
 const authCtrl = require("./auth.controller");
+const { LoginDTO } = require("./auth.validator");
 
 const authRouter = require("express").Router();
 
 // authRouter.post("/register", "");
 // authRouter.get("/activate/:token", "" );
-authRouter.post("/register", authCtrl.registerUser);
+authRouter.post("/register", bodyValidator(LoginDTO), authCtrl.registerUser);
 authRouter.get("/login", authCtrl.loginUser);
+authRouter.get("/me", checkLogin, authCtrl.getLoggedInUser);
 
 module.exports = authRouter;
