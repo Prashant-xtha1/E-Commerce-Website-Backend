1aba9e208b08d1287d6d5829082bda04423d096e
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-05-29 09:48:34 +0545

    Registration DTO add

--- src/modules/authentication/auth.router.js
+++ src/modules/authentication/auth.router.js
@@ -1,14 +1,14 @@
 const checkLogin = require("../../middlewares/auth.middleware");
 const bodyValidator = require("../../middlewares/validation.middleware");
 const authCtrl = require("./auth.controller");
-const { LoginDTO } = require("./auth.validator");
+const { LoginDTO, RegisterDTO } = require("./auth.validator");
 
 const authRouter = require("express").Router();
 
 // authRouter.post("/register", "");
 // authRouter.get("/activate/:token", "" );
-authRouter.post("/register", bodyValidator(LoginDTO), authCtrl.registerUser);
-authRouter.get("/login", authCtrl.loginUser);
+authRouter.post("/register", bodyValidator(RegisterDTO), authCtrl.registerUser);
+authRouter.get("/login", bodyValidator(LoginDTO), authCtrl.loginUser);
 authRouter.get("/me", checkLogin, authCtrl.getLoggedInUser);
 
 module.exports = authRouter;
--- src/modules/authentication/auth.validator.js
+++ src/modules/authentication/auth.validator.js
@@ -6,6 +6,16 @@ const LoginDTO = Joi.object({
   password: Joi.string().min(8).max(25).required()
 });
 
+const RegisterDTO = Joi.object({
+  name: Joi.string().min(2).max(50).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+  role: Joi.string().valid("user", "seller").default("user"),
+  image: Joi.string().allow(null, '').optional().default(null)
+})
+
 module.exports = {
-  LoginDTO
+  LoginDTO,
+  RegisterDTO
 }
