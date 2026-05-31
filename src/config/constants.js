a5f8d8f9e5ce5aea1d9d7f76691eefc8d74053e5
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-05-31 21:55:30 +0545

    Constant data create

---
+++ src/config/constants.js
@@ -0,0 +1,15 @@
+const UserRoles = {
+  ADMIN = "admin",
+  CUSTOMER = "customer",
+  SELLER = "seller",
+}
+
+const Status = {
+  ACTIVE = "active",
+  INACTIVE = "inactive",
+}
+
+module.exports = {
+  UserRoles,
+  Status
+}
