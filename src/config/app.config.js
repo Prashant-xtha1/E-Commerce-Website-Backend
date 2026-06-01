dd1d4e3eb50377bbe50ae8890900e338d4f1c7b5
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-06-01 11:56:12 +0545

    app config create

---
+++ src/config/app.config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const CloudinaryConfig = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_API_KEY,
+  apiSecret: process.env.CLOUDINARY_API_SECRET,
+}
+
+module.exports = {
+  CloudinaryConfig
+}
