60cb7f5f01fe2405f55c7e2a332e4de786365f2a
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-06-13 16:53:05 +0545

    randomStringGenerator helper create

---
+++ src/utilities/helper.js
@@ -0,0 +1,15 @@
+const generateRandomString = (len=100) => {
+  const chars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const charsLen = chars.length;
+  let random = "";
+
+  for(let i=0; i<len; i++){
+    const posn = Math.ceil(Math.random() * (charsLen-1));
+    random += chars[posn]
+  }
+  return random;
+}
+
+module.exports = {
+  generateRandomString,
+}
