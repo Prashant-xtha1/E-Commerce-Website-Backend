c338e829c9ef4b14250f8e92b983aa6e765b9910
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-06-13 16:52:41 +0545

    auth service creation

---
+++ src/modules/authentication/auth.service.js
@@ -0,0 +1,26 @@
+const cloudinaryService = require("../../services/cloudinary.service");
+const bycrpt = require("bcryptjs");
+const { generateRandomString } = require("../../utilities/helper");
+
+class AuthService {
+  async tranformForUser(req) {
+    try {
+      const data = req.body;
+      data.password = bcrypt.hashSync(data.password, 12);
+      if(req.file) {
+        data.image = await cloudinaryService.singleFileUpload(req.file.path, "/users");
+      }
+      
+      // Token creation
+      data.token = generateRandomString();
+      data.expiryTime = Date.now() + 86400000
+
+      return data;
+    } catch (exception) {
+      throw exception;
+    }
+  }
+}
+
+const authService = new AuthService();
+module.exports = authService;
