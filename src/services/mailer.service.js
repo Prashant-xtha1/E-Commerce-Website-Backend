e8d615d2c0bc19146d82bedaf11f1dbedb282d03
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-06-15 13:47:56 +0545

    update mail service

--- src/services/mailer.service.js
+++ src/services/mailer.service.js
@@ -23,4 +23,34 @@ class EmailService {
       }
     }
   }
+
+  async sendEmail({to, subject, message, cc=null, bcc=null, attachments=null}) {
+    try {
+      let messageBody = {
+        to: to,
+        from: SMTPConfig.smtpFrom,
+        subject: subject,
+        html: message,
+      };
+      if(cc) {
+        messageBody["cc"] = cc;
+      }
+      if(bcc) {
+        messageBody["bcc"] = bcc;
+      }
+      if(attachments) {
+        messageBody["attachments"] = attachments;
+      }
+      return await this.#transport.sendMail(messageBody);
+    } catch (exception) {
+      throw {
+        code: 500,
+        message: "Email Cannot Be Sent",
+        status: "SMTP_MAIL_SENT_ERR",
+      }
+    }
+  }
 }
+
+const emailService = new EmailService();
+module.exports = emailService;
