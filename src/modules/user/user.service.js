4f4598083c66ae2c14ae40715453e7607b825873
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-06-21 23:21:31 +0545

    user public profile function creation

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -1,15 +1,29 @@
 const UserModel = require("./user.model");
 
 class UserService {
-  async storeUser (data) {
+  async storeUser(data) {
     try {
       const user = new UserModel(data);
       return await user.save();
     } catch (exception) {
-      throw exception
+      throw exception;
     }
+  }
+
+  getPublicProfileOfUser(user) {
+    const userObj = {
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      image: user.image,
+      status: user.status,
+      _id: user._id,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+    return userObj;
   }
 }
 
-const userService =  new UserService();
+const userService = new UserService();
 module.exports = userService;
