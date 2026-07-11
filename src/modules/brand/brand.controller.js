92e568861da4371193ab905bec009bb0d53acbd1
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-11 17:13:29 +0545

    create brandController

---
+++ src/modules/brand/brand.controller.js
@@ -0,0 +1,52 @@
+class BrandController {
+  async create(req, res, next) {
+    try {
+      
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async listAll(req, res, next) {
+    try {
+      
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async getDetail(req, res, next) {
+    try {
+      
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async getDetailBySlug(req, res, next) {
+    try {
+      
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+      
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      
+    } catch (exception) {
+      next(exception);
+    }
+  }
+}
+
+const brandCtrl = new BrandController();
+module.exports = brandCtrl;
