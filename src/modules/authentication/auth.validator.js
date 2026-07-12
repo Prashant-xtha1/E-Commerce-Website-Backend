820812a89301219eabfe382b2f74ecff04eb4c4b
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-12 22:27:57 +0545

    add password regex

--- src/modules/authentication/auth.validator.js
+++ src/modules/authentication/auth.validator.js
@@ -7,10 +7,12 @@ const LoginDTO = Joi.object({
   password: Joi.string().min(8).max(25).required()
 });
 
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W@-_]).{8,}$/
+
 const RegisterDTO = Joi.object({
   name: Joi.string().min(2).max(50).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().regex(passwordRegex).required(),
   confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
   role: Joi.string().regex(/^(customer|seller)$/).default(UserRoles.CUSTOMER),
   image: Joi.string().allow(null, '').optional().default(null)
