c3c15a3346299660da003128cecc5659f1a515b7
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-23 00:19:18 +0545

    add categoryRouter in Router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,11 @@
 const authRouter = require("../modules/authentication/auth.router");
 const brandRouter = require("../modules/brand/brand.router");
+const categoryRouter = require("../modules/category/category.router");
 
 const router = require("express").Router();
 
 router.use("/auth", authRouter);
 router.use("/brand", brandRouter);
+router.use("/category", categoryRouter);
 
 module.exports = router;
