ccdf55ad4e242cf348d96f5eb74a1531dc858d1e
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-23 00:17:57 +0545

    feat: update category model

--- src/modules/category/category.model.js
+++ src/modules/category/category.model.js
@@ -1,44 +1,59 @@
 const mongoose = require("mongoose");
 const { Status } = require("../../config/constants");
 
-const CategorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 100,
-    required: true,
-    unique: true,
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 100,
+      required: true,
+      unique: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(Status),
+      default: Status.INACTIVE,
+    },
+    image: {
+      publicId: String,
+      url: String,
+      optimizedUrl: String,
+    },
+    parentId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+      default: null,
+    },
+    brandId: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Brand",
+        default: null,
+      },
+    ],
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    updatedBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
   },
-  slug: {
-    type: String,
-    unique: true,
-    required: true,
+  {
+    timestamps: true,
+    autoCreate: true,
+    autoIndex: true,
   },
-  status: {
-    type: String,
-    enum: Object.values(Status),
-    default: Status.INACTIVE,
-  },
-  logo: {
-    publicId: String,
-    url: String,
-    optimizedUrl: String,
-  },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    default: null,
-  },
-  updatedBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    default: null,
-  },
-}, {
-  timestamps: true,
-  autoCreate: true,
-  autoIndex: true,
-})
+);
 
 const CategoryModel = mongoose.model("Category", CategorySchema);
 module.exports = CategoryModel;
