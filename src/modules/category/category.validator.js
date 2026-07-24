dc1584964c58b19e315e28d62c4ace0909fde7e0
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-24 22:21:08 +0545

    feat: add parentId and brandId in validator

--- src/modules/category/category.validator.js
+++ src/modules/category/category.validator.js
@@ -4,8 +4,10 @@ const { Status } = require("../../config/constants");
 
 const CategoryDTO = Joi.object({
   name: Joi.string().min(2).max(100).required(),
+  parentId: Joi.string().optional().allow(null, "").default(null),
+  brandId: Joi.array().items(Joi.string()).optional().allow(null, "").default(null),
   status: Joi.string().regex(/^(active|inactive)$/).default(Status.INACTIVE),
-  logo: Joi.string().allow(null, "").optional().default(null),
+  image: Joi.string().allow(null, "").optional().default(null),
 })
 
 module.exports = {
