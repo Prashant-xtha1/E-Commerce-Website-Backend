50f8fa2d49eb3378d03158ba3c5e09e6ee211d78
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-28 22:58:19 +0545

    feat: add product validation using Joi

--- src/modules/product/product.validator.js
+++ src/modules/product/product.validator.js
@@ -1,7 +1,22 @@
 const Joi = require("joi");
+const { Status } = require("../../config/constants");
 
 const ProductDTO = Joi.object({
-
+  name: Joi.string().min(3).max(200).required(),
+  price: Joi.number().min(1).required(),
+  discount: Joi.number().min(0).max(95).required(),
+  description: Joi.string().min(100).required(),
+  brand: Joi.string().optional().allow(null, "").default(null),
+  category: Joi.array().items(Joi.string()).required(),
+  stock: Joi.number().min(0).optional().allow(null, "").default(0),
+  sku: Joi.string().optional().allow(null, "").default(null),
+  attributes: Joi.array().items(Joi.object({
+    key: Joi.string(),
+    attributes: Joi.array().items(Joi.string())
+  })).optional().allow(null, "").default(null),
+  seller: Joi.string().optional().allow(null, "").default(null),
+  status: Joi.string().regex(/^(active|inactive)$/).default(Status.INACTIVE),
+  images: Joi.string().optional().allow(null, "").default(null),
 })
 
 module.exports = {
