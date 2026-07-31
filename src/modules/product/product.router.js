a1ba65540b45b6ea14ac6c518190292267cc60e7
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-31 22:01:38 +0545

    feat: create product listing router api

--- src/modules/product/product.router.js
+++ src/modules/product/product.router.js
@@ -8,6 +8,6 @@ const { ProductDTO } = require("./product.validator");
 const productRouter = require("express").Router();
 
 productRouter.post("/", checkLogin([UserRoles.SELLER]), uploader().array("images"), bodyValidator(ProductDTO), productCtrl.createProduct);
-
+productRouter.get("/", checkLogin([UserRoles.SELLER]), productCtrl.getAllProducts);
 
 module.exports = productRouter;
