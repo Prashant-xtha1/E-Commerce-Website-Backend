78335012d4d18dad52e6f50f6a880278aef1cfe0
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-31 22:01:05 +0545

    feat: create product list method

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -1,3 +1,4 @@
+const { UserRoles } = require("../../config/constants");
 const productService = require("./product.service");
 
 class ProductController {
@@ -17,6 +18,24 @@ class ProductController {
     }
   }
 
+  async getAllProducts(req, res, next) {
+    try {
+      let filter = {};
+
+      if(req.loggedInUser !== UserRoles.ADMIN){
+        filter = {
+          createdBy: req.loggedInUser._id;
+        }
+      }
+
+      // search
+      if(req.quer)
+
+      const data = await productService.getAllRowsByFilter()
+    } catch (exception) {
+      next(exception);
+    }
+  }
 }
 
 const productCtrl = new ProductController();
