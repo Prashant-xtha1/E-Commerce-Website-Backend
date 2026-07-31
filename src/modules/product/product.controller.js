f1406ed2b4bf1e2c53be67c4ab41fd8fb876d36e
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-31 12:32:53 +0545

    feat: add createProduct method

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -1,4 +1,21 @@
+const productService = require("./product.service");
+
 class ProductController {
+
+  async createProduct(req, res, next) {
+    try {
+      const data = await productService.transformToProduct(req);
+      const product = await productService.storeProduct(data);
+
+      res.json({
+        data: product,
+        message: "Product Created Successfully",
+        status: "SUCCESS",
+      })
+    } catch (exception) {
+      next(exception);
+    }
+  }
 
 }
 
