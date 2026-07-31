e8198e78e3a6440cab4007a6b4fc3d91c0a28a1a
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-07-31 22:02:48 +0545

    feat: add product routing api

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,13 @@
 const authRouter = require("../modules/authentication/auth.router");
 const brandRouter = require("../modules/brand/brand.router");
 const categoryRouter = require("../modules/category/category.router");
+const productRouter = require("../modules/product/product.router");
 
 const router = require("express").Router();
 
 router.use("/auth", authRouter);
 router.use("/brand", brandRouter);
 router.use("/category", categoryRouter);
+router.use("/product", productRouter);
 
 module.exports = router;
