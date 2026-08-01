0f42d719ae40fd9b1e31067694601f296684b82a
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-08-01 14:36:11 +0545

    feat(product): implement product listing with filtering and pagination

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -22,16 +22,45 @@ class ProductController {
     try {
       let filter = {};
 
-      if(req.loggedInUser !== UserRoles.ADMIN){
+      if(req.loggedInUser.role !== UserRoles.ADMIN){
         filter = {
-          createdBy: req.loggedInUser._id;
+          createdBy: req.loggedInUser._id
         }
       }
 
       // search
-      if(req.quer)
+      if(req.query.search) {
+        filter = {
+          ...filter,
+          $or: [
+            {name: new RegExp(req.query.search, "i")},
+            {description: new RegExp(req.query.search, "i")}
+          ]
+        }
+      }
 
-      const data = await productService.getAllRowsByFilter()
+      // filter by status
+      if(req.query.status) {
+        filter = {
+          ...filter,
+          status: req.query.status,
+        }
+      }
+
+      // pagination
+      const page = +req.query.page || 1;
+      const limit = +req.query.limit || 20;
+
+      const {data, pagination} = await productService.getAllRowsByFilter(filter, {page: page, limit: limit});
+
+      res.json({
+        data: data,
+        message: "Product fetched successfully",
+        status: "SUCCESS",
+        meta: {
+          pagination
+        }
+      })
     } catch (exception) {
       next(exception);
     }
