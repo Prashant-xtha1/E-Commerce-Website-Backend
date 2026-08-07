9caefa753f11fac69f46e24825ec835d062d8b95
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-08-07 15:13:51 +0545

    implementing orderRouter path to route

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,7 @@
 const authRouter = require("../modules/authentication/auth.router");
 const brandRouter = require("../modules/brand/brand.router");
 const categoryRouter = require("../modules/category/category.router");
+const orderRouter = require("../modules/order/order.router");
 const productRouter = require("../modules/product/product.router");
 
 const router = require("express").Router();
@@ -9,5 +10,6 @@ router.use("/auth", authRouter);
 router.use("/brand", brandRouter);
 router.use("/category", categoryRouter);
 router.use("/product", productRouter);
+router.use("/order", orderRouter);
 
 module.exports = router;
