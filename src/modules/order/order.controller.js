28ee841e5e694c1169953dde50c93ed77f98a121
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-08-15 14:17:09 +0545

    feat(order): add validation and cartDetail fetch method

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -12,13 +12,20 @@ class OrderController {
         _id: product
       })
 
+      if(!productInfo) {
+        throw {
+          code: 404,
+          message: "Product Not Found",
+          status: "PRODUCT_NOT_FOUND_ERR",
+        }
+      }
+
+      let cart = await orderService.getSingleRowByFilter({
+        status: "cart",
+        buyer: loggedInUser._id
+      })
       
-
-      // orderDetail according to the model
-      let orderDetail = [orderService.createOrderDetail(productInfo, quantity)];
-      console.log(orderDetail);
-
-      let orderCalculation = {
+      let orderCostCalculation = {
         subTotal: 0,
         serviceCharge: 0,
         discount: 0,
@@ -26,17 +33,34 @@ class OrderController {
         total: 0,
       };
 
+      let orderDetail = null;
+      let msg = "";
       let transaction = null;
+      let order = null;
 
-      let orderInfo = {
-        orderId: generateRandomString(15),
-        buyer: loggedInUser._id,
-        detail: orderDetail,
-        ...orderCalculation,
-        transaction: transaction,
-        status: "cart",
-        createdBy: loggedInUser._id,
+      // if cart exists in db then update cart 
+      if(cart) {
+        orderDetail = cart.detail;
+      } else {
+        // create cart if not exists in db
+        orderDetail = [orderService.createOrderDetail(productInfo, quantity)];
+        orderCostCalculation = orderService.createOrderCostCalculation(orderDetail);
+        
+        let orderInfo = {
+          orderId: generateRandomString(15),
+          buyer: loggedInUser._id,
+          detail: orderDetail,
+          ...orderCostCalculation,
+          transaction: transaction,
+          status: "cart",
+          createdBy: loggedInUser._id,
+        }
+
+        order = await orderService.createOrder(orderInfo);
       }
+      
+
+
 
       res.json({
         data: orderInfo,
