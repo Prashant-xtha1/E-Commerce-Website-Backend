1abaf8be87ed5b6726ae21871863ae930906cc6e
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-08-16 21:29:37 +0545

    feat(order): add cartListing method route

--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -7,5 +7,6 @@ const orderCtrl = require("./order.controller");
 const orderRouter = require("express").Router();
 
 orderRouter.post("/add-to-cart", checkLogin([UserRoles.ADMIN, UserRoles.SELLER]), bodyValidator(AddToCartDTO), orderCtrl.addToCart);
+orderRouter.get("/cart-list", checkLogin([UserRoles.ADMIN, UserRoles.CUSTOMER]),  )
 
 module.exports = orderRouter;
