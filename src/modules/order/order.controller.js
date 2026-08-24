518704cf68910249af2d5b1c5d1c457f73286db7
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-08-24 22:05:18 +0545

    feat: add updateOrRemoveFromCart controller

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -148,6 +148,100 @@ class OrderController {
       next(exception);
     }
   }
+
+  async updateOrRemoveFromCart (req, res, next) {
+    try {
+      const cartId = req.params.cartId;
+      const loggedInUser = req.loggedInUser;
+      const {product, quantity} = req.body;
+
+      let cart = await orderService.getSingleRowByFilter({
+        _id: cartId,
+        buyer: loggedInUser._id,
+        status: "cart"
+      });
+
+      if(!cart) {
+        throw {
+          code: 404,
+          message: "Cart not found",
+          status: "CART_NOT_FOUND_ERR",
+        }
+      }
+
+      const productDetail = await productService.getSingleRowByFilter({
+        _id: product
+      })
+
+      if(!productDetail) {
+        throw {
+          code: 404,
+          message: "Product not found",
+          status: "PRODUCT_NOT_FOUND_ERR",
+        }
+      }
+
+      let index = null;
+      cart.detail.map((item, ind) => {
+        if(item.product._id.equals(product)) {
+          index = ind;
+        };
+      });
+
+      if(index === null) {
+        throw {
+          code: 404,
+          message: "Product doesnot exists in cart anymore",
+          status: "PRODUCT_ITEM_NOT_FOUND_ERR",
+        }
+      }
+
+      let cartItems = cart.detail;
+      let orderDetail = cartItems[index];
+
+      if(orderDetail.quantity < quantity) {
+        throw {
+          code: 422,
+          message: "Quantity is less in your cart",
+          status: "LESS_QUANTITY_IN_CART_ERR",
+        }
+      } else if (+quantity === 0 || +quantity === orderDetail.quantity) {
+        // remove quantity
+        cartItems.splice(index, 1);
+      } else {
+        cartItems[index].quantity -= +quantity;
+        cartItems[index].price = productDetail.afterDiscount;
+        cartItems[index].subTotal = cartItems[index].quantity * cartItems[index].price;
+      }
+
+      let msg = "";
+
+      if(cartItems.length > 0) {
+        // update cart
+        const updateBody = {
+          detail: cartItems,
+          ...orderService.createOrderCostCalculation(cartItems)
+        }
+        cart = await orderService.updateSingleRowByFilter({_id: cartId}, updateBody)
+        msg = "Your cart updated successfully";
+      } else {
+        // delete cart
+        cart = await orderService.deleteSingleRowByFilter({_id: cartId});
+        msg = "Your cart deleted successfully";
+      }
+
+      res.json({
+        data: cart,
+        message: msg,
+        status: "SUCCESS",
+      })
+
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+
 }
 
 const orderCtrl = new OrderController();
