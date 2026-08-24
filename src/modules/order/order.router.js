06ec576084bad59413571b6b416edd439adb411a
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-08-24 22:04:37 +0545

    feat:  add cart-update route

--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -1,12 +1,13 @@
 const { UserRoles } = require("../../config/constants");
 const checkLogin = require("../../middlewares/auth.middleware");
 const bodyValidator = require("../../middlewares/validation.middleware");
-const { AddToCartDTO } = require("./order.contract");
+const { AddToCartDTO, UpdateCartDTO } = require("./order.contract");
 const orderCtrl = require("./order.controller");
 
 const orderRouter = require("express").Router();
 
 orderRouter.post("/add-to-cart", checkLogin([UserRoles.ADMIN, UserRoles.SELLER]), bodyValidator(AddToCartDTO), orderCtrl.addToCart);
-orderRouter.get("/cart-list", checkLogin([UserRoles.ADMIN, UserRoles.CUSTOMER]),  )
+orderRouter.get("/cart-list", checkLogin([UserRoles.ADMIN, UserRoles.CUSTOMER]), orderCtrl.getMyCartList);
+orderRouter.patch("/cart-update/:cartId", checkLogin([UserRoles.ADMIN, UserRoles.CUSTOMER]), bodyValidator(UpdateCartDTO), orderCtrl.updateOrRemoveFromCart)
 
 module.exports = orderRouter;
