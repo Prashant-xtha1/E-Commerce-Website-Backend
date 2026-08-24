f468b52215dbdc0446df564f2a58a08b7e334458
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-08-24 22:30:53 +0545

    feat: create order checkout router

--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -1,13 +1,15 @@
 const { UserRoles } = require("../../config/constants");
 const checkLogin = require("../../middlewares/auth.middleware");
 const bodyValidator = require("../../middlewares/validation.middleware");
-const { AddToCartDTO, UpdateCartDTO } = require("./order.contract");
+const { AddToCartDTO, UpdateCartDTO, CheckoutDTO } = require("./order.contract");
 const orderCtrl = require("./order.controller");
 
 const orderRouter = require("express").Router();
 
 orderRouter.post("/add-to-cart", checkLogin([UserRoles.ADMIN, UserRoles.SELLER]), bodyValidator(AddToCartDTO), orderCtrl.addToCart);
 orderRouter.get("/cart-list", checkLogin([UserRoles.ADMIN, UserRoles.CUSTOMER]), orderCtrl.getMyCartList);
-orderRouter.patch("/cart-update/:cartId", checkLogin([UserRoles.ADMIN, UserRoles.CUSTOMER]), bodyValidator(UpdateCartDTO), orderCtrl.updateOrRemoveFromCart)
+orderRouter.patch("/cart-update/:cartId", checkLogin([UserRoles.ADMIN, UserRoles.CUSTOMER]), bodyValidator(UpdateCartDTO), orderCtrl.updateOrRemoveFromCart);
+
+orderRouter.post("/checkout", checkLogin([UserRoles.ADMIN, UserRoles.CUSTOMER]), bodyValidator(CheckoutDTO), orderCtrl.checkoutOrder);
 
 module.exports = orderRouter;
