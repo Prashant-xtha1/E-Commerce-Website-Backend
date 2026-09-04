990fe451d68a481a511417a39873bbe618007cf1
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-09-04 23:47:41 +0545

    feat: add listing users router

--- src/modules/chat/chat.router.js
+++ src/modules/chat/chat.router.js
@@ -1,3 +1,8 @@
+const checkLogin = require("../../middlewares/auth.middleware");
+const chatCtrl = require("./chat.controller");
+
 const chatRouter = require("express").Router();
+
+chatRouter.get("/list-users", checkLogin(), chatCtrl.listAllUsers);
 
 module.exports = chatRouter;
