808e83539c802162d30a51f6c956cf24005f8c99
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-09-04 23:47:14 +0545

    feat: create getAllUsersByFilter method

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -19,6 +19,31 @@ class UserService {
     }
   }
 
+  async getAllUsersByFilter (filter, config) {
+    try {
+      const skip = (config.page - 1) * config.limit;
+      const data = await UserModel.find(filter)
+      .sort({"name": "asc"})
+      .skip(skip)
+      .limit(config.limit)
+
+      const total = await UserModel.countDocuments(filter);
+
+      return {
+        data: data.map(user => this.getPublicProfileOfUser(user)),
+        pagination: {
+          page: +config.page,
+          limit: +config.limit,
+          total: total,
+          noOfPages: Math.ceil(total/config.limit)
+        }
+      }
+
+    } catch (exception) {
+      throw exception;
+    }
+  }
+
   async updateSingleRowByFilter(filter, data){
     try {
       const update = await UserModel.findOneAndUpdate(filter, {$set: data}, {new: true})
