5836366d3baabe2e2a019d0756def997b47590ba
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-09-04 23:17:55 +0545

    feat(router): add chatRouter in routerJs

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,7 @@
 const authRouter = require("../modules/authentication/auth.router");
 const brandRouter = require("../modules/brand/brand.router");
 const categoryRouter = require("../modules/category/category.router");
+const chatRouter = require("../modules/chat/chat.router");
 const orderRouter = require("../modules/order/order.router");
 const productRouter = require("../modules/product/product.router");
 
@@ -11,5 +12,6 @@ router.use("/brand", brandRouter);
 router.use("/category", categoryRouter);
 router.use("/product", productRouter);
 router.use("/order", orderRouter);
+router.user("/chat", chatRouter);
 
 module.exports = router;
