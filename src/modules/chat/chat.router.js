c92919e6c357a4ba70aa178f313e1f787777fb5d
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-09-05 10:46:21 +0545

    feat: add send-message route api

--- src/modules/chat/chat.router.js
+++ src/modules/chat/chat.router.js
@@ -1,8 +1,11 @@
 const checkLogin = require("../../middlewares/auth.middleware");
+const bodyValidator = require("../../middlewares/validation.middleware");
 const chatCtrl = require("./chat.controller");
+const { SendChatDTO } = require("./chat.validator");
 
 const chatRouter = require("express").Router();
 
 chatRouter.get("/list-users", checkLogin(), chatCtrl.listAllUsers);
+chatRouter.post("/send-message", checkLogin(), bodyValidator(SendChatDTO), chatCtrl.sendMessage);
 
 module.exports = chatRouter;
